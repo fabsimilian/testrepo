c1d7597e980789e03b709312a6a9a0b1f90f7d79
Fabian Schmid <fs@tailored-apps.com>
2015-08-11 14:56:09 +0200

    added button

--- TestProject/app/src/main/java/com/fabsimilian/testproject/MainActivity.java
+++ TestProject/app/src/main/java/com/fabsimilian/testproject/MainActivity.java
@@ -4,13 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private Button mButton;
+    int mClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText("Clicked: "+ ++mClicked);
+            }
+        });
+
     }
 
     @Override
@@ -34,4 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
